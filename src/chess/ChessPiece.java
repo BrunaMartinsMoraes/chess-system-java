df27f2ea06bc27b6b9c3c014003c8054a496276c
Bruna Martins <brunaamartinsmorae@gmail.com>
2022-10-11 14:47:33 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -20,6 +20,12 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
+    //Methods
+
+    public ChessPosition getChessPosition() {
+        return ChessPosition.fromPosition(position);
+    }
+
     protected boolean isThereOpponentPiece(Position position) {
         ChessPiece p = (ChessPiece)getBoard().piece(position);
         return p != null && p.getColor() != color;
